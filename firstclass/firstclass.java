bcdb1de1a75efd89eaaa0a167bd7d26574889aaa
Janvi101102 <125116391+Janvi101102@users.noreply.github.com>
2024-05-23 15:33:19 +0530

    Add files via upload

---
+++ firstclass/firstclass.java
@@ -0,0 +1,20 @@
+package firstclass;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		
+		int a=20,b=40,ans;
+		
+		ans = a + b; 
+		System.out.println("Addition => "+a+"+"+b+"="+ ans);
+		ans = b - a;
+		System.out.println("Subtraction => "+b+"-"+a+"="+ ans);
+		ans = a * b;
+		System.out.println("Multiplication => "+a+"*"+b+"="+ ans);
+		ans = a / b;
+		System.out.println("Division => "+a+"/"+b+"="+ ans);
+
+	}
+
+}
