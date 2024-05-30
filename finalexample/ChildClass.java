95cdbba6e476a30900ba8fb7f8f268a287bd3de9
Janvi101102 <125116391+Janvi101102@users.noreply.github.com>
2024-05-31 00:00:29 +0530

    Add files via upload

---
+++ finalexample/ChildClass.java
@@ -0,0 +1,9 @@
+package finalexample;
+
+public class ChildClass extends FinalVariable{
+	
+	@Override
+	public final void show() {
+		System.out.println("I am final function");
+	}
+}
---
+++ finalexample/FinalVariable.java
@@ -0,0 +1,18 @@
+package finalexample;
+
+final class Test{
+	
+}
+
+public class FinalVariable {
+	final int salary;
+	
+	public FinalVariable() {
+		salary = 145;
+	}
+	
+	public final void show() {
+		//Test te = new Test();
+		System.out.println("I am final function");
+	}
+}
